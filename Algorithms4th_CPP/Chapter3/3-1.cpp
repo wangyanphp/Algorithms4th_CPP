f0b842ff74e887a807219776336f809d79fb3735
wangshanshi <wangshanshi@WANGSHANSHI-PC>
2018-03-02 17:27:39 +0800

    更新第三章

--- Algorithms4th_CPP/Chapter3/3-1.cpp
+++ Algorithms4th_CPP/Chapter3/3-1.cpp
@@ -1,4 +1,4 @@
-#include"ST.h"
+#include"st.h"
 #include<iostream>
 #include<string>
 
@@ -34,39 +34,39 @@ void countFrequency(std::istream& in, int minLen)
 
 			
 }
-
-/**
- *3.1.25:缓存
- *if (!st.contains(word)) st.put(word, 1);
-	else st.put(word, st.get(word) + 1);
- *这种代码会在内循环中将同一个键查找两三遍
- *为了提高这种用例代码的效率，我们可以使用一种叫做缓存的技术，即：将访问最频繁的键的位置保存在一个变量中。
- *
- *对于SequentialSearchST，我们可以使用3.1.22自组织查找，这是一种将数组元素重新排序使得被访问频率较高的元素更容易被找到的查找算法
- *这样的话，在每次查找命中时，将被找到的键值对移动到数组的开头即可
- *
- *对于BinarySearchST，，，，，
- */
-
-/**
- *3.1.28 有序插入，修改BinarySearchST，使得插入一个比当前所有键都大的键只需要常数时间
- *如此，则有序构造符号表时，只需要线性时间
- */
-	//std::ifstream in("E:\\tale.txt");
-
-	//countFrequency(in, 8);
-	
-	/*
-	std::vector<int> a{ 6, 3, 8, 1, 2, 4, 5, 7, 9, 0 };
-	BST<int, int> st;
-	for (int i = 0; i < a.size(); ++i)
-		st.put(a[i], a[i]);
-
-	st.deleteMin();
-	st.deleteMax();
-	st.deleteKey(6);
-	auto v = st.toArray();
-
-	for (auto item : v)
-		std::cout << item.key << '\t' << item.value << std::endl;
-		*/
+//
+///**
+// *3.1.25:缓存
+// *if (!st.contains(word)) st.put(word, 1);
+//	else st.put(word, st.get(word) + 1);
+// *这种代码会在内循环中将同一个键查找两三遍
+// *为了提高这种用例代码的效率，我们可以使用一种叫做缓存的技术，即：将访问最频繁的键的位置保存在一个变量中。
+// *
+// *对于SequentialSearchST，我们可以使用3.1.22自组织查找，这是一种将数组元素重新排序使得被访问频率较高的元素更容易被找到的查找算法
+// *这样的话，在每次查找命中时，将被找到的键值对移动到数组的开头即可
+// *
+// *对于BinarySearchST，，，，，
+// */
+//
+///**
+// *3.1.28 有序插入，修改BinarySearchST，使得插入一个比当前所有键都大的键只需要常数时间
+// *如此，则有序构造符号表时，只需要线性时间
+// */
+//	//std::ifstream in("E:\\tale.txt");
+//
+//	//countFrequency(in, 8);
+//	
+//	/*
+//	std::vector<int> a{ 6, 3, 8, 1, 2, 4, 5, 7, 9, 0 };
+//	BST<int, int> st;
+//	for (int i = 0; i < a.size(); ++i)
+//		st.put(a[i], a[i]);
+//
+//	st.deleteMin();
+//	st.deleteMax();
+//	st.deleteKey(6);
+//	auto v = st.toArray();
+//
+//	for (auto item : v)
+//		std::cout << item.key << '\t' << item.value << std::endl;
+//		*/
--- Algorithms4th_CPP/Chapter3/3-3.cpp
+++ Algorithms4th_CPP/Chapter3/3-3.cpp
@@ -1,8 +1,80 @@
-#include"ST.h"
+#include"st.h"
+#include<string>
+#include<iostream>
+using std::string;
 
 /**
  *3.3.29 最优存储：实现无需为结点颜色的存储使用额外空间：要将结点标记为红色，只需要交换它的左右链接
 
  *
  */
+//int main()
+//{
+//	BST<int, string> st;
+//	st.put(1, "wangyan");
+//	st.put(2, "zhangpengcheng");
+//	st.put(3, "wangshanshi");
+//	st.put(4, "april");
+//
+//	std::cout << st.get(4)->second << std::endl;
+//	std::cout << st.floor(3)->second << std::endl;
+//	std::cout << st.ceiling(2)->second << std::endl;
+//
+//	std::cout << st.rank(3) << std::endl;
+//
+//
+//	st.deleteKey(3);
+//
+//
+//	for (auto item:st)
+//		std::cout << item.first << ":" << item.second << std::endl;
+//
+//	system("pause");
+//}
 
+//class A
+//{
+//public:
+//	void f() { ff(); }
+//	virtual void ff() { std::cout << "AA" << std::endl; }
+//};
+//
+//class B :public A
+//{
+//public:
+//	void f() { A::f(); }
+//	virtual void ff() { std::cout << "BB" << std::endl; }
+//};
+//
+//
+//int main()
+//{
+//	B b;
+//	b.f();
+//	system("pause");
+//}
+
+//int main()
+//{
+//	RedBlackBST<int, string> st;
+//	st.put(1, "wangyan");
+//	st.put(2, "zhangpengcheng");
+//	st.put(3, "wangshanshi");
+//	st.put(4, "april");
+//
+//	std::cout << st.get(4)->second << std::endl;
+//	std::cout << st.floor(3)->second << std::endl;
+//	std::cout << st.ceiling(2)->second << std::endl;
+//
+//	std::cout << st.rank(3) << std::endl;
+//
+//
+//	//if (st.is23Tree()) 
+//	{
+//		for (auto item : st) { std::cout << item.first << ":" << item.second << std::endl; }
+//
+//	}
+//
+//
+//	system("pause");
+//}
